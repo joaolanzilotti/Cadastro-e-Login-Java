6480f0d49c2fc66014f48c0e53ddaeccf183976b
João Pedro Lanzilotti Ferretti <71307203+joaosinho2@users.noreply.github.com>
2022-08-02 13:28:21 -0300

    Add files via upload

---
+++ src/main/java/com/mycompany/cadastroeloginnovo/CadastroMain.java
@@ -0,0 +1,12 @@
+
+
+package com.mycompany.cadastroeloginnovo;
+
+import GUI.CadastroGUI;
+import GUI.LoginGUI;
+public class CadastroMain {
+
+    public static void main(String[] args) {
+        new LoginGUI().setVisible(true);
+    }
+}
